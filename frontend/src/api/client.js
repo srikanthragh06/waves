bdd53fd868e452ec09c0a36a6cb634184aa854cc
srikanthragh06 <srikanthragh06@gmail.com>
2024-03-20 17:30:22 +0530

    added /api/ instead /api in client.js in frontend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
     process.env.NODE_ENV === "production"
-        ? "api/"
+        ? "/api/"
         : "http://localhost:5000/api/";
 
 const client = axios.create({ baseURL });
